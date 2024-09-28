117c3853d7da6029ac4b4d59d56f7bbcb523e31e
nigelabrera0026 <abrera.nigel0026@gmail.com>
2024-09-27 20:43:22 -0500

    Attempt deployment #3

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+
+// https://vitejs.dev/config/
+export default defineConfig( ({}) => {
+  const config = {
+    plugins: [react()],
+    base: process.env.BUILD_PATH_PREFIX || undefined
+  }
+
+  if (process.env.BUILD_COMBINED === "true" && process.env.BUILD_TYPE === "storybook") {
+    return {
+      ...config,
+      base: config.base ? config.base + "/moviebait" : "/moviebait"
+    }
+  }
+
+  return {
+    ...config
+  }
+})
