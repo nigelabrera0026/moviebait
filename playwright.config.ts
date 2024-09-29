1b4b74e5fa96d529bffa915258bba0fb2d491484
nigelabrera0026 <abrera.nigel0026@gmail.com>
2024-09-28 22:43:31 -0500

    Configured playwright, attempting deployment

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './tests',  // This is where all test files will be placed
+  testDir: './src/tests',  // This is where all test files will be placed
   retries: 1,          // Retries failed tests once
   use: {
     baseURL: 'http://localhost:5173',  // Your app's URL (for Vite, it's localhost:5173)
